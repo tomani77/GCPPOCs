bf76a2b9721a7769f1818b3e0c8386dbc680693c
tomani77 <tomani77@gmail.com>
2025-04-05 18:08:49 +0530

    Update search_restaurant.py

--- vectorsearch/search_restaurant.py
+++ vectorsearch/search_restaurant.py
@@ -3,18 +3,20 @@ from google.cloud import aiplatform
 from google.cloud import storage
 import vertexai
 from vertexai.language_models import TextEmbeddingModel
-from utility import load_data_from_bucket
+from utility import load_data_from_bucket,upload_embeddings_bucket, get_embeddings_wrapper
+from google import genai
+import tqdm
 
 # GCP Configuration
 PROJECT_ID = "agentic-ai-poc"  # Replace with your GCP project ID
 LOCATION = "us-central1"
 BUCKET_NAME = "manish-synthetic" # Ensure bucket exists
 EMBEDDING_MODEL_NAME = "text-embedding-005" #"textembedding-gecko@001" # Or your preferred model
-EMBEDDED_FILE_NAME = "synthetic_restaurants_embedded.json"
+EMBEDDED_FILE_NAME = "synthetic_restaurants.json"
 
 # Index and Endpoint details
-DEPLOYED_INDEX_ID = "restaurant_index_deploy_1743277591859" # Replace with your Index ID
-INDEX_ENDPOINT_ID = "229538445461028864" # Replace with your Endpoint ID
+DEPLOYED_INDEX_ID = "rest_deployed_1743845886277" # Replace with your Index ID
+INDEX_ENDPOINT_ID = "2001564161342963712" # Replace with your Endpoint ID
 
 vertexai.init(project=PROJECT_ID, location=LOCATION)
 restaurants_data = load_data_from_bucket(BUCKET_NAME, EMBEDDED_FILE_NAME, PROJECT_ID)
@@ -31,27 +33,29 @@ def query_vector_search(query):
     Returns:
         A list of matching restaurant results.
     """
-    embeddings = embedding_model.get_embeddings([query])
-    query_embedding = [embedding.values for embedding in embeddings][0]
+    # embeddings = embedding_model.get_embeddings([query])
+    # query_embedding = [embedding.values for embedding in embeddings][0]
+    # #print('query embedding ' + str(query_embedding))
 
-    #print('query embedding ' + str(query_embedding))
-
-    # index_endpoint = aiplatform.MatchingEngineIndexEndpoint(ENDPOINT_ID)
-
+    query_embeddings = get_embeddings_wrapper([query])
+    
     index_endpoint = aiplatform.MatchingEngineIndexEndpoint(
             index_endpoint_name=f"projects/{PROJECT_ID}/locations/{LOCATION}/indexEndpoints/{INDEX_ENDPOINT_ID}",
             project=PROJECT_ID,
             location=LOCATION,
         )
 
+    # response = index_endpoint.find_neighbors(
+    #     deployed_index_id=DEPLOYED_INDEX_ID,
+    #     queries=[query_embedding],
+    #     num_neighbors=5,  # Adjust as needed
+    # )
+
     response = index_endpoint.find_neighbors(
         deployed_index_id=DEPLOYED_INDEX_ID,
-        queries=[query_embedding],
+        queries=query_embeddings,
         num_neighbors=5,  # Adjust as needed
     )
-
-    # print("Response object:", response)  # Inspect the response
-    # print("Type of response:", type(response)) # Inspect the type of response
 
     # Extract the list of MatchNeighbor objects directly
     neighbors = response[0]  # Access the first (and only) inner list
@@ -60,14 +64,14 @@ def query_vector_search(query):
 
     # Iterate through the MatchNeighbor objects and extract the data
     for neighbor in neighbors:
-        result_index = int(neighbor.id)
+        result_index = int(neighbor.id) -1 
         print (neighbor.id + " "  + str(neighbor.distance))
         # print("Neighbor ID:", neighbor.distance)
         results.append(restaurants_data[result_index])
 
     return results
 
-query = "List of Japanese cuisine restaurants"
+query = "find  veg Indian restaurant which dont serve non veg food in area 60601"
 search_results = query_vector_search(query)
 
 print("Search Results:")
@@ -82,6 +86,6 @@ for restaurant in search_results:
         "kids_friendly": restaurant["kids_friendly"],
         "wheelchair_accessible": restaurant["wheelchair_accessible"],
         "average_rating": restaurant["average_rating"],
-        "credit_card_benefits": restaurant.get("credit_card_benefits", [])
+        # "credit_card_benefits": restaurant.get("credit_card_benefits", [])
     })
 
